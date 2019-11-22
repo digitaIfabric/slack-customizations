575ed39f679429cbeba090589fbabe8ac915cf61
Digital Fabric / David <dwawryko@gmail.com>
2019-11-22 14:37:08 -0500

    Created wysiwyg disabling function

---
+++ disable-wysiwyg.js
@@ -0,0 +1,17 @@
+  
+(() => {
+    const workspaceIds = slackDebug.clientStore.workspaces.getAllWorkspaces();
+    for (const workspaceId of workspaceIds) {
+        const { redux } = slackDebug[workspaceId];
+        const {
+            wysiwyg_composer,
+            wysiwyg_composer_ios,
+            wysiwyg_composer_webapp,
+            ...payload
+        } = redux.getState().experiments;
+        redux.dispatch({
+            type: '[19] Bulk add experiment assignments to redux',
+            payload
+        });
+    }
+})();
